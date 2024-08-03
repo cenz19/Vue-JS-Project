4f0ca86ff72f781e73d6b714747868a6838687de
cenz19 <vincenthadinata19@gmail.com>
2024-08-03 14:09:07 +0700

    Initial push

---
+++ vue-crash-2024/vue-crash-2024/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+//ini setup biar ga ngecoding export default nya (lebih pendek)
+import { ref } from "vue";
+//using composition API
+// export default {
+//   setup() {
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const link = ref("https://www.google.com");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+// return {
+//   name,
+//   status,
+//   tasks,
+//   link,
+//   toggleStatus,
+// };
+//   },
+// };
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <!-- di JS disuru biasanya e. prevent -->
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" />
+  </form>
+
+  <h3>Task</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a v-bind:href="link">Click for google</a>
+  <a :href="link">Click for google</a>
+
+  <!-- <button v-on:click="toggleStatus">change status</button> -->
+  <button @click="toggleStatus">change status</button>
+</template>
